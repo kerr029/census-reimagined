b04543d1cdbf24521ea90b64c000368b14a4b9c6
Rex Kerr <rexkerr29@gmail.com>
2021-05-06 18:19:56 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -26,39 +26,4 @@ $(function () {
             minimumResultsForSearch: -1
         }).val(null).trigger('change');
     })
-
-   /* $(function () {
-        //Checkboves Dynamic ID create
-        var checkboxDataId = 0;
-        $(".checkbox-group, .form-check").each(function () {
-            checkboxDataId++;
-            $('input', this).attr("id", "check-" + checkboxDataId);
-            $('.form-check-label', this).attr("for", "check-" + checkboxDataId);
-        });
-    });*/
 });
-
-
-/*
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 600 && y < 1000) {
-        $('#sticky-nav ul li').css("color", "white");
-        $('#sticky-nav ul #sec-1-nav').css("color", "black");
-    } else if (y > 1000 && y < 1600) {
-        $('#sticky-nav ul li').css("color", "white");
-        $('#sticky-nav ul #sec-2-nav').css("color", "black");
-    } else if (y > 1600 && y < 2100) {
-        $('#sticky-nav ul li').css("color", "white");
-        $('#sticky-nav ul #sec-3-nav').css("color", "black");
-    }  else if (y > 1900 && y < 2400) {
-        $('#sticky-nav ul li').css("color", "white");
-        $('#sticky-nav ul #sec-4-nav').css("color", "black");
-    }  else if (y > 2400 && y < 2900) {
-        $('#sticky-nav ul li').css("color", "white");
-        $('#sticky-nav ul #sec-5-nav').css("color", "black");
-    }  else if (y > 2900 && y < 3300) {
-        $('#sticky-nav ul li').css("color", "white");
-        $('#sticky-nav ul #sec-6-nav').css("color", "black");
-    }
-});*/
